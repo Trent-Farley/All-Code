52d4b8d39640cd2c7e289e44a66f62dc8b25f4a7
Trent-Farley <trent.a.farley@gmail.com>
2019-04-30 10:54:26 -0700

    stkuff

--- CS260/Assignment_3/link_list.hpp
+++ CS260/Assignment_3/link_list.hpp
@@ -51,6 +51,4 @@ class linked_list{
 
     }
 
-
-
 };
--- CS260/Assignment_3/main.cpp
+++ CS260/Assignment_3/main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
-#include "../use/Node.hpp"
+#include "link_list.hpp"
+using std::cout;
+using std::endl; 
+void show(node *head){// Display the current stack
 
+    if(head == nullptr)
+    {
+        cout << "NULL" << endl;
+    }
+    else
+    {
+        cout << head->num << endl;
+        show(head->next);
+    }
+}
 int main(){
-    std::cout<<"Alls good"<<std::endl;
-    return 23;
+    linked_list list;
+    for(int i; i<5; i++){
+        list.add_nodes(i,i+2);
+    }
+    list.add_nodes(10, 2);
+    show(list.get_head());
+
+
 }
