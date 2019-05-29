46cd831326f559f6fa385200217a071724e0ddb2
Trent-Farley <trent.a.farley@gmail.com>
2019-05-29 16:10:17 -0700

    This works

--- CS260/Assignment_5/tree.hpp
+++ CS260/Assignment_5/tree.hpp
@@ -1,7 +1,12 @@
+//I know iostream is not good to include, but I 
+//just want to finish faster. I could easily have the functions
+//return a value to be picked up and checked, but I already spent 
+//enough time on this. 
 #include <iostream>
-#include <cstdlib>
+
 using std::cout;
-class tree{
+using std::endl;
+class Tree{
     private:
         struct tree_node
         {
@@ -12,14 +17,14 @@ class tree{
         tree_node* root;
     
     public:
-        tree()
-        {
+        Tree(){
            root = NULL; //Started Using NULL because it makes more sense
         }
        
-        bool isEmpty() const {
-             return root== NULL;
+        bool isempty() const {
+            return root == NULL;
         }
+
         void print_inorder();
         void inorder(tree_node*);
         void print_preorder();
@@ -32,8 +37,7 @@ class tree{
 
 // Smaller elements go left
 // larger elements go right
-void tree::insert(int d)
-{
+void Tree::insert(int d){
     tree_node* tree = new tree_node;
     tree_node* parent;
     tree->data = d;
@@ -41,35 +45,40 @@ void tree::insert(int d)
     tree->right = NULL;
     parent = NULL;
     
-    // is this a new tree?
-    if(isEmpty()) root = t;
-    else
-    {
+    // Look to see if it is empty
+    if(isempty()){ 
+        root = tree;    
+    }
+
+    else{
         //Note: ALL insertions are as leaf nodes
         tree_node* curr;
         curr = root;
-        // Find the Node's parent
-        while(curr)
-        {
+        // Find the node's parent
+        while(curr){
+
             parent = curr;
             if(tree->data > curr->data) curr = curr->right;
             else curr = curr->left;
         }
 
-        if(tree->data < parent->data)
-           parent->left = t;
-        else
-           parent->right = t;
+        if(tree->data < parent->data){
+           parent->left = tree;
+        }
+
+        else{
+           parent->right = tree;
+        }
     }
 }
 
-void tree::remove(int num)
+void Tree::remove(int num)
 {
     //Locate the element
     bool found = false;
-    if(isEmpty())
-    {
-        cout<<" this tree is empty! "<<endl;
+    if(isempty()){
+
+        cout<<" Empty tree "<<endl;
         return;
     }
     
@@ -84,16 +93,20 @@ void tree::remove(int num)
             found = true;
             break;
          }
-         else
-         {
-             parent = curr;
-             if(num>curr->data) curr = curr->right;
-             else curr = curr->left;
+         else{
+
+            parent = curr;
+            if(num>curr->data) {
+                curr = curr->right;
+            }
+            else{
+                curr = curr->left;
+            }
          }
     }
     if(!found)
 		 {
-        cout<<" Data not found! "<<endl;
+        cout<<" Nothing found "<<endl;
         return;
     }
 
@@ -105,44 +118,49 @@ void tree::remove(int num)
     */
     //Node with single chld... I dont like this. 
     if((curr->left == NULL && curr->right != NULL)|| (curr->left != NULL \
-    && curr->right == NULL))
-    {
-       if(curr->left == NULL && curr->right != NULL)
-       {
-           if(parent->left == curr)
-           {
-             parent->left = curr->right;
-             delete curr;
+    && curr->right == NULL)){
+
+
+       if(curr->left == NULL && curr->right != NULL){
+           
+           if(parent->left == curr){
+             
+            parent->left = curr->right;
+            delete curr;
            }
-           else
-           {
-             parent->right = curr->right;
-             delete curr;
+           else{
+            
+            parent->right = curr->right;
+            delete curr;
            }
        }
-       else // left child present, no right child
-       {
-          if(parent->left == curr)
-           {
-             parent->left = curr->left;
-             delete curr;
+       else {// left child present, no right child
+       
+          if(parent->left == curr){
+             
+            parent->left = curr->left;
+            delete curr;
            }
-           else
-           {
-             parent->right = curr->left;
-             delete curr;
+           else{
+
+            parent->right = curr->left;
+            delete curr;
            }
        }
      return;
     }
 
 		 //leaf node
-		 if( curr->left == NULL && curr->right == NULL)
-    {
-        if(parent->left == curr) parent->left = NULL;
-        else parent->right = NULL;
-		 		 delete curr;
-		 		 return;
+		 if( curr->left == NULL && curr->right == NULL){
+
+        if(parent->left == curr) {
+            parent->left = NULL;
+        }
+        else {
+            parent->right = NULL;
+		 	delete curr;
+		 	return;
+        }
     }
 
 
@@ -158,31 +176,31 @@ void tree::remove(int num)
             delete check;
             curr->right = NULL;
         }
-        else // right child has children
-        {
-            //Go to left if the right child has a left child.
-            if((curr->right)->left != NULL)
-            {
+        else { // right child has children
+             //left if the right child has a left child.
+            if((curr->right)->left != NULL){
+
                 tree_node* lcurr;
                 tree_node* lcurrp;
                 lcurrp = curr->right;
                 lcurr = (curr->right)->left;
-                while(lcurr->left != NULL)
-                {
-                   lcurrp = lcurr;
-                   lcurr = lcurr->left;
+
+                while(lcurr->left != NULL){
+                
+                    lcurrp = lcurr;
+                    lcurr = lcurr->left;
                 }
-		curr->data = lcurr->data;
+		        curr->data = lcurr->data;
                 delete lcurr;
                 lcurrp->left = NULL;
            }
-           else
-           {
-               tree_node* tmp;
-               tmp = curr->right;
-               curr->data = tmp->data;
-	        curr->right = tmp->right;
-               delete tmp;
+           else{
+
+                tree_node* tmp;
+                tmp = curr->right;
+                curr->data = tmp->data;
+	            curr->right = tmp->right;
+                delete tmp;
            }
 
         }
@@ -191,50 +209,51 @@ void tree::remove(int num)
 
 }
 
-void tree::print_inorder()
-{
+void Tree::print_inorder(){
+  
   inorder(root);
 }
 
-void tree::inorder(tree_node* p)
-{
-    if(p != NULL)
-    {
+void Tree::inorder(tree_node *p){
+    
+    if(p != NULL){
+        
         if(p->left) inorder(p->left);
-        cout<<" "<<p->data<<" ";
+        cout<<" << "<<p->data<<" << ";
         if(p->right) inorder(p->right);
     }
     else return;
 }
 
-void tree::print_preorder()
-{
+void Tree::print_preorder(){
+    
     preorder(root);
 }
 
-void tree::preorder(tree_node* p)
-{
-    if(p != NULL)
-    {
-        cout<<" "<<p->data<<" ";
+void Tree::preorder(tree_node* p){
+    
+    if(p != NULL){
+        
+        cout<<" << "<<p->data<<" << ";
         if(p->left) preorder(p->left);
         if(p->right) preorder(p->right);
     }
     else return;
 }
 
-void tree::print_postorder()
-{
+void Tree::print_postorder(){
+    
     postorder(root);
 }
 
-void tree::postorder(tree_node* p)
-{
-    if(p != NULL)
-    {
+void Tree::postorder(tree_node *p){
+    
+    if(p != NULL){
+
         if(p->left) postorder(p->left);
         if(p->right) postorder(p->right);
-        cout<<" "<<p->data<<" ";
+    
+        cout<<" << "<<p->data<<" << ";
     }
     else return;
 }
