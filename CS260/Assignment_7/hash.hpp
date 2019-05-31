72dc3928fc12abfe92a3a915f824dcd205ee94c1
Trent-Farley <trent.a.farley@gmail.com>
2019-05-31 07:49:50 -0700

    Works

--- CS260/Assignment_7/hash.hpp
+++ CS260/Assignment_7/hash.hpp
@@ -1,119 +1,129 @@
+#include <iostream>
+using std::cout;
+using std::endl;
 struct node{
     int data;
     node *next;
     node *prev;
-};
+    };
 
 class Hash_Table{
     private:
         bool exists = false;
         int size[20];
 
-        
-    public: 
+
+    public:
         node **arr = new node*[20];
         node *head;
         node *tail;
-        
 
-        Hash_Table(){
-            head = new node;
-            tail = new node;
-            head->next = NULL;
-            head->prev = NULL;
-            tail->prev = NULL;
-            tail->next = NULL;
-            head->data = 1;
-            tail->data = 1;
-            for(int i = 0; i<21; i++){
-                size[i] = 0;
-            }
 
+    Hash_Table(){
+        head = new node;
+        tail = new node;
+        head->next = NULL;
+        head->prev = NULL;
+        tail->prev = NULL;
+        tail->next = NULL;
+        head->data = 1;
+        tail->data = 1;
+        for(int i = 0; i<21; i++){
+            size[i] = 0;
         }
-        node *create(int val){
-            node *new_node = new node;
-            new_node->next = NULL;
-            new_node->prev = NULL;
-            new_node->data = val;
-            return new_node;
-        }
-
-        int *linked_list(int data){
-            int i = 0;
-            int pos = data;
-            node *new_node = create(data);
-            node *curr;
-            curr = arr[pos];
-            if(size[0] == 0){
-                head->next = new_node;
-                new_node->prev = arr[pos];
-                new_node->next = NULL;
-                tail->prev = new_node;
-                size[0] = 1;
-                return size;
-            }
-            else{
-                while(i<=pos){
-                    
-                    if(new_node->data <= curr->next->data || i == 20){
-                        new_node->next = curr->next;
-                        new_node->prev = curr; 
-                        curr->next->prev = new_node;
-                        curr->next = new_node;
-                        i=pos+1;
-                        
-                            
-                    }
-
-                    else{
-                        curr = curr->next;
-                    }
-                    i++;
-                
-                }
-
-            }
-            size[pos]++;
-            return size;     
-        }
-        node **find_and_put(int val){
-            
-            if(exists == false){
-                for(int i = 0; i<21; i++){
-                    arr[i] = create(0);
-                }
-                arr[0]->data = 1;
-                arr[20]->data = 1;
-                exists = true;
-                return arr;
-
-            }   
-            else if(val == 0){
-                arr[val+1] = create(val);
-                return arr;
-            }
-            else if(arr[val]->data==0){
-                arr[val] = create(val);
-                return arr;
-            }
-            else if(val == arr[val]->data){
-                linked_list(val);
-                return arr;
-            }
-            else if(val>arr[19]->data){
-                if(arr[19]->data == 0){
-                    arr[19] = create(val);
-                }
-                else{
-                    linked_list(val);
-                }
-                return arr;
-            }
-            else{
-                arr[val] = create(val);
-                return arr;
-            }
 
     }
+    node *create(int val){
+        node *new_node = new node;
+        new_node->next = NULL;
+        new_node->prev = NULL;
+        new_node->data = val;
+        return new_node;
+    }
 
-};
+    int *linked_list(int data){
+        int i = 0;
+        int pos = data;
+        node *new_node = create(data);
+        node *curr;
+        curr = arr[pos];
+
+        if(size[pos] == 0){
+            curr->next = new_node;
+            new_node->prev = curr;
+            new_node->next = NULL;
+            
+            size[pos]++;
+            return size;
+        }
+        else{
+            while(i<=pos){
+                
+                if(new_node->data <= curr->next->data || curr->next == NULL){
+                    new_node->next = curr->next;
+                    new_node->prev = curr;
+                    curr->next->prev = new_node;
+                    curr->next = new_node;
+                    i=pos+1;
+                    
+                        
+                }
+
+                else{
+                    curr = curr->next;
+                }
+                i++;
+            
+            }
+            size[pos]++;
+            return size;  
+        }
+
+    }
+    node **find_and_put(int val){
+
+        if(exists == false){
+            int follower = 0;
+            for(int i = 0; i<21; i++){
+                arr[i] = create(0);
+            }
+            arr[0]->data = 1;
+            arr[20]->data = 1;
+
+            exists = true;
+            if(val<20){
+                arr[val]->data = val;
+                return arr;
+            }
+
+        }  
+        else if(val == 0){
+            arr[val+1] = create(val);
+            return arr;
+        }
+        else if(arr[val]->data==0){
+            arr[val] = create(val);
+            return arr;
+        }
+        else if(val == arr[val]->data){
+            linked_list(val);
+            return arr;
+        }
+        else if(val>arr[19]->data){
+            if(arr[19]->data == 0){
+                arr[19] = create(val);
+            }
+            else{
+                linked_list(val);
+            }
+            return arr;
+        }
+        else{
+            arr[val] = create(val);
+            return arr;
+        }
+
+        }
+
+    };
+
--- CS260/Assignment_7/main.cpp
+++ CS260/Assignment_7/main.cpp
@@ -5,18 +5,29 @@ using std::cout;
 using std::endl;
 
 int main(){
+    
     Hash_Table table;
-    node **example = table.find_and_put(5);
-    node **sec_example = table.find_and_put(5);
-    //node **sec_example = table.find_and_put(12);
+    table.find_and_put(12);
+    table.find_and_put(12);
+    table.find_and_put(12);
+    node **example = table.find_and_put(0);
+    
+        
+
+    
     for(int i=0; i<21; i++){
-    
         cout<<" << "<<example[i]->data;
+        if(example[i]->next!=NULL){
+            node *curr = example[i];
+            while(curr != NULL){
+                cout<<endl;
+                
+                cout<<example[i]->next->data<<endl;
+                curr = curr->next;
+            }
+        }
     }
-    
-    
+
     cout<<endl;
-    cout<<sec_example[5]->data<<endl;
-    
 
 }
